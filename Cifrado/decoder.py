6d83d42d0eeb0f2d2544c36729581751ce26e7d1
Alejandro Ramos <arhcoder@gmail.com>
2020-11-03 22:00:27 -0600

    Información de creación agregada.

--- Cifrado/decoder.py
+++ Cifrado/decoder.py
@@ -1,3 +1,9 @@
+# Desarrollador: Alejandro Ramos Herrera (@arhcoder).
+# Licencia: Creative Commons - Attribution-NonCommercial 4.0 International (CC BY-NC 4.0).
+# Desarrollado en noviembre de 2020.
+# Tiempo de desarrollo: X.
+# Propósito: Práctica y Entretenimiento.
+
 # S = {0, ..., 55295} - {0, 7, ... 10, 13, 27, 128 ... 160};
 NOusables = [0, 13, 27] + list(range(7, 11)) + list(range(128, 161))
 # Total = 55,256;
